3b4fca6432be7ab90589c5939754efffb51e8785
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-10 15:27:40 -0500

    Clase 3 - Crear controlador con Spring Boot + AI con LangChain4j

---
+++ src/main/java/com/platzi/play/PlatziPlayAiService.java
@@ -0,0 +1,14 @@
+package com.platzi.play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de Gestión de Películas PlatziPlay. 
+            Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
+            """)
+    String generateGreeting();
+}
