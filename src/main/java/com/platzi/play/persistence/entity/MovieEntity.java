06d235a778aff55357c44d2d7404ef4956d7bf00
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-11 10:25:30 -0500

    Clase 7 - Crear un Entity referenciado a una tabla

---
+++ src/main/java/com/platzi/play/persistence/entity/MovieEntity.java
@@ -0,0 +1,93 @@
+package com.platzi.play.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "platzi_play_peliculas")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String titulo;
+
+    @Column(nullable = false, precision = 3)
+    private Integer duracion;
+
+    @Column(nullable = false, length = 40)
+    private String genero;
+
+    @Column(name = "fecha_estreno")
+    private LocalDate fechaEstreno;
+
+    @Column(precision = 3, scale = 2)
+    private BigDecimal clasificacion;
+
+    @Column(nullable = false, length = 1)
+    private String estado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(LocalDate fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public BigDecimal getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(BigDecimal clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
