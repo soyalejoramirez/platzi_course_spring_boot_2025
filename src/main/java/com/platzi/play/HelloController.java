9641627d358549f761af13f068d049f5eb9a166c
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-11 09:22:41 -0500

    Clase 4 - Configuración de Spring Boot con application.properties

--- src/main/java/com/platzi/play/HelloController.java
+++ src/main/java/com/platzi/play/HelloController.java
@@ -1,18 +1,21 @@
 package com.platzi.play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final String plataform;
     private final PlatziPlayAiService aiService;
 
-    public HelloController(PlatziPlayAiService aiService) {
+    public HelloController(@Value("${spring.application.name}") String plataform, PlatziPlayAiService aiService) {
+        this.plataform = plataform;
         this.aiService = aiService;
     }
 
     @GetMapping("/")
     public String hello() {
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(plataform);
     }
 }
--- src/main/java/com/platzi/play/PlatziPlayAiService.java
+++ src/main/java/com/platzi/play/PlatziPlayAiService.java
@@ -1,14 +1,15 @@
 package com.platzi.play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Gestión de Películas PlatziPlay. 
+            Genera un saludo de bienvenida a la plataforma de Gestión de Películas {{plataform}}.
             Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("plataform") String plataform);
 }
