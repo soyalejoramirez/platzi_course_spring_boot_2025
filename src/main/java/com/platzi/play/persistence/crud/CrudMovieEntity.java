fb10d6f1e2d49a6957d534c73fd60c8c6e69d62b
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-11 10:56:00 -0500

    Clase 8 - Crear Repository para operaciones CRUD

---
+++ src/main/java/com/platzi/play/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,7 @@
+package com.platzi.play.persistence.crud;
+
+import com.platzi.play.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+}
