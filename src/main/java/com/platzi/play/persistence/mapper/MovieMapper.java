229f7dcc799b3f33a390ded28d418cf03435c4ff
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-14 10:26:54 -0500

    Clase 16 - Crear endpoint POST para añadir un nuevo recurso

--- src/main/java/com/platzi/play/persistence/mapper/MovieMapper.java
+++ src/main/java/com/platzi/play/persistence/mapper/MovieMapper.java
@@ -2,6 +2,7 @@ package com.platzi.play.persistence.mapper;
 
 import com.platzi.play.domain.dto.MovieDto;
 import com.platzi.play.persistence.entity.MovieEntity;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -16,4 +17,8 @@ public interface MovieMapper {
     @Mapping(source = "clasificacion", target = "rating")
     MovieDto toDto(MovieEntity entity);
     List<MovieDto> toDto(Iterable<MovieEntity> entities);
+
+    @InheritInverseConfiguration
+    @Mapping(source = "genre", target = "genero", qualifiedByName = "genreToString")
+    MovieEntity toEntity(MovieDto dto);
 }
